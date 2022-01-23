288cc855a4bf32fd776031d79e49207643fb11dc
Hailey Cooperrider <haileycooperrider@gmail.com>
2022-01-23 13:53:43 +1100

    set shuffled.vue default data sync to 200 rows of spreadsheet

--- src/components/formats/Shuffled.vue
+++ src/components/formats/Shuffled.vue
@@ -832,7 +832,7 @@ export default {
       var getURL =
         "https://sheets.googleapis.com/v4/spreadsheets/" +
         sheetID +
-        "?includeGridData=true&ranges=a1:aa100&key=" +
+        "?includeGridData=true&ranges=a1:aa200&key=" +
         process.env.VUE_APP_FIREBASE_API_KEY;
 
       // For the published version - remove if you're hardcoding the data instead of using Google Sheets

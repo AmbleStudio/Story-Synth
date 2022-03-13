c3879a6f475487b37868af3ededca9e33c20343a
Hailey Cooperrider <haileycooperrider@gmail.com>
2022-03-14 10:56:13 +1100

    fix merge issues in Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -7,11 +7,7 @@
     v-if="!$route.params.roomID"
     :class="{ 'shadow scrolledNavbar': scrollPosition > 100 }"
   >
-<<<<<<< Updated upstream
     <b-navbar-brand href="/">Play.Amble</b-navbar-brand>
-=======
-    <b-navbar-brand href="/">Story Synth</b-navbar-brand>
->>>>>>> Stashed changes
     <b-navbar-toggle
       target="navbarSupportedContent"
       aria-controls="navbarSupportedContent"
@@ -22,19 +18,9 @@
 
     <b-collapse id="navbarSupportedContent" is-nav>
       <b-navbar-nav>
-<<<<<<< Updated upstream
         <b-nav-item href="https://amble.studio"
           >Back to Amble.Studio</b-nav-item
         >
-=======
-        <b-nav-item href="https://docs.storysynth.org/guide">About</b-nav-item>
-        <b-nav-item href="/Gallery/">Gallery</b-nav-item>
-        <b-nav-item href="/Upload/">Upload</b-nav-item>
-        <b-nav-item href="https://docs.storysynth.org/guide/design.html"
-          >Guide</b-nav-item
-        >
-        <b-nav-item href="https://docs.storysynth.org/press/">Press</b-nav-item>
->>>>>>> Stashed changes
       </b-navbar-nav>
     </b-collapse>
     <div>Login</div>
@@ -61,8 +47,4 @@ export default {
     window.removeEventListener("scroll", this.updateScroll);
   },
 };
-<<<<<<< Updated upstream
 </script>
-=======
-</script>
->>>>>>> Stashed changes

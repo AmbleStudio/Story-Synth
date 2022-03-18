0b2f71b0ba7ba6923ba900f04c254f9a9b4193ce
Hailey Cooperrider <haileycooperrider@gmail.com>
2022-03-18 11:04:00 +1100

    broken attempt to make auth and authui work with firebase7, now abandoned

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,42 @@
 import {
   initializeApp
 } from 'firebase/app';
-import "firebase/auth";
+import * as firebaseauth from "firebase/auth";
+import * as firebaseui from "firebaseui";
 
 const app = initializeApp({
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: "",
+  authDomain: "ss-amble.firebaseapp.com",
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: "",
-  messagingSenderId: ""
+  storageBucket: "ss-amble.appspot.com",
+  messagingSenderId: "750012918172"
 });
+
+export const auth = app.auth();
+/* eslint-disable no-debugger */
+debugger
+
+export const authUiConfig = {
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebaseauth.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+};
+
+//*REFERENCE*/
+// export const authUiConfig = {
+//   signInSuccessUrl: "/",
+//   signInOptions: [{
+//     provider: app.auth.EmailAuthProvider.PROVIDER_ID,
+//     customParameters: {
+//       prompt: "select_account",
+//     },
+//   }, ],
+// };
+
+export const authUi = new firebaseui.auth.AuthUI(auth);
+
 
 export const db = app.firestore();
 

0d379ff6fbaee5c8db125e281eb5b6e2d322a00d
Hailey Cooperrider <haileycooperrider@gmail.com>
2022-04-29 09:43:48 +1000

    add dev config option to config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,17 @@
+// export default {
+//   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+//   //authDomain: "ss-amble.firebaseapp.com",
+//   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
+//   //storageBucket: "ss-amble.appspot.com"
+//   //messagingSenderId: "750012918172",
+//   //appId: "1:750012918172:web:b33c9a0198c2ae9cf0c059"
+// };
+
+// dev version
+
 export default {
+<<<<<<< Updated upstream
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   //authDomain: "ss-amble.firebaseapp.com",
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
@@ -7,3 +20,12 @@ export default {
   //messagingSenderId: "750012918172",
   //appId: "1:750012918172:web:b33c9a0198c2ae9cf0c059"
 };
+=======
+  apiKey: "AIzaSyC8wD5AVrPHhbmPP48jGqkLMz1sV8xzf9k",
+  authDomain: "ss-amble-dev.firebaseapp.com",
+  projectId: "ss-amble-dev",
+  storageBucket: "ss-amble-dev.appspot.com",
+  messagingSenderId: "81574765944",
+  appId: "1:81574765944:web:4031b86ccdac88c18c948d"
+}
+>>>>>>> Stashed changes

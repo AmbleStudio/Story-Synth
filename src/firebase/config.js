c8af41a6cc77d7ddf97d9e4feb23e45dd6db199b
Hailey Cooperrider <haileycooperrider@gmail.com>
2022-05-04 13:39:01 +1000

    commit changes to config.js from end of last work session

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,7 @@
+// TODO: Double check this is the correct config
+
+// *** prod version ***
+
 // export default {
 //   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
 //   //authDomain: "ss-amble.firebaseapp.com",
@@ -8,24 +12,14 @@
 //   //appId: "1:750012918172:web:b33c9a0198c2ae9cf0c059"
 // };
 
-// dev version
+// *** dev version ***
 
 export default {
-<<<<<<< Updated upstream
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  //authDomain: "ss-amble.firebaseapp.com",
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
-  //storageBucket: "ss-amble.appspot.com"
-  //messagingSenderId: "750012918172",
-  //appId: "1:750012918172:web:b33c9a0198c2ae9cf0c059"
-};
-=======
   apiKey: "AIzaSyC8wD5AVrPHhbmPP48jGqkLMz1sV8xzf9k",
   authDomain: "ss-amble-dev.firebaseapp.com",
+  databaseURL: "https://ss-amble-dev-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "ss-amble-dev",
   storageBucket: "ss-amble-dev.appspot.com",
   messagingSenderId: "81574765944",
   appId: "1:81574765944:web:4031b86ccdac88c18c948d"
 }
->>>>>>> Stashed changes

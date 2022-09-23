cc275b4f63db87c2dbc91cbdf240432cdc349a6a
Hailey Cooperrider <haileycooperrider@gmail.com>
2022-09-23 14:01:57 +1000

    remove a Story Synth reference

--- src/components/launchers/GameLauncher.vue
+++ src/components/launchers/GameLauncher.vue
@@ -70,7 +70,7 @@
             !customOptions.gameCover
           ">
             <div class="col-sm text-center">
-              <h1>Playtest with Story Synth</h1>
+              <h1>Playtest with Play.Amble</h1>
               <div class="col-sm">
                 This session will use the {{ routeGameType }} template and
                 <a v-bind:href="
